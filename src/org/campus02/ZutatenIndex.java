dbf6eb0d0768a3b69441130c4a7d78147958021f
MarioWintes <52406123@edu.campus02.at>
2024-10-20 18:37:21 +0200

    testt

--- src/org/campus02/ZutatenIndex.java
+++ src/org/campus02/ZutatenIndex.java
@@ -14,6 +14,8 @@ public class ZutatenIndex {
         mehlSeiten.add(2);
         mehlSeiten.add(1230);
         mehlSeiten.add(8);
+        mehlSeiten.add(8);
+        mehlSeiten.add(8);
         mehlSeiten.add(10); // Initialisierung!!
 
         zutatenIndex.put("mehl", mehlSeiten);
